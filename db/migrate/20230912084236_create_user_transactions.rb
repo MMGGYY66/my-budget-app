4e73b9c02975322d3d431b2fd519e6634fcfa82d
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-12 11:46:56 +0300

    Create UserTransaction model

---
+++ db/migrate/20230912084236_create_user_transactions.rb
@@ -0,0 +1,13 @@
+class CreateUserTransactions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_transactions do |t|
+      t.references :category, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: 'users' }
+
+      t.string :name
+      t.float :amount
+
+      t.timestamps
+    end
+  end
+end

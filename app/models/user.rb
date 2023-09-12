27490fc96bb7a9a8d89b3f403ab18069d38c603d
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-12 11:51:13 +0300

    Update User  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
-  has_many :expenses, dependent: :destroy, foreign_key: :author_id
-
   validates :name, presence: true
-
-  protected
-
-  def password_required?
-    false
-  end
+  has_many :categories, foreign_key: 'user_id'
+  has_many :user_transactions, foreign_key: :author_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_081904) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_084236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_081904) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "user_transactions", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_user_transactions_on_author_id"
+    t.index ["category_id"], name: "index_user_transactions_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -30,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_081904) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "user_transactions", "categories"
+  add_foreign_key "user_transactions", "users", column: "author_id"
 end

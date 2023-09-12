01b22d28b390ba9a45973145b79a507ae9e3bede
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-12 18:58:30 +0300

    Add devise User views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
-  belongs_to :user
-  has_many :user_transactions
-
   validates :name, presence: true
   validates :icon, presence: true
+  belongs_to :user
+  has_many :user_transactions
 end
